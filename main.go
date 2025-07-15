1d51b975ba6044bfb57ba9e7166131700e712072
Morten Idemar Andersen Tenmann <mortenidemar@gmail.com>
2025-07-15 19:39:54 +0200

    update main

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
@@ -24,10 +25,20 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	if r.Method != "GET" {
-
+		http.Error(w, "metod is not supported", http.StatusNotFound)
+		return
 	}
 	fmt.Fprintf(w, "hello!")
 }
 
 func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Printf("Starting server at port: 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
